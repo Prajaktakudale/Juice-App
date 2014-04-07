6bd396ec5786b86b896d51274369a7bb243df72c
Prajakta Kudale <prajaktakudale@INPrajakta.local>
2014-04-07 18:42:45 +0530

    Service API completed.

--- demo1.rb
+++ demo1.rb
@@ -1,18 +1,23 @@
 require 'sinatra'
 require 'mongoid'
 require 'json'
+require './model/employee.rb'
+require './model/juice_request.rb'
 Mongoid.load!("./config/mongoid.yml")
 
-get '/employee/:id/drinks/:numberOfGlasses/glass' do
-  "#{params[:id]} - #{params[:numberOfGlasses]}"
+get '/total/numberOfGlasses' do
+	"#{Employee.all.collect(&:juice_requests).flatten.length}"
 end
 
 post '/employee/:id/drinks/:numberOfGlasses/glass' do
-  new_juice_request = JSON.parse(request.body.read)
-  employee_id = new_juice_request["employee_id"]
-  numberOfGlasses = new_juice_request["numberOfGlasses"]
-  date_time = new_juice_request["date_time"]
-  
-  request = Employee.new(:employee_id => employee_id, :juice_requests => [JuiceRequest.new({:no_of_glasses => numberOfGlasses, :date_time => Date.today})])
-  request.save!
+  	new_juice_request = JSON.parse(request.body.read)
+  	emp_id = new_juice_request["employee_id"]
+  	numberOfGlasses = new_juice_request["numberOfGlasses"]
+  	date_time = new_juice_request["date_time"]
+  	if(Employee.where({:employee_id => emp_id}).exists?)
+  		Employee.find_by(employee_id: "15749").juice_requests << JuiceRequest.new({:no_of_glasses => numberOfGlasses,:date_time => date_time})
+  	else
+  		request = Employee.new(:employee_id => emp_id, :juice_requests => [JuiceRequest.new({:no_of_glasses => numberOfGlasses, :date_time => date_time})])
+  		request.save!
+	end
 end
