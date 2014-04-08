28ee7caf1ad73bd7bd0f7df21ac0f1c787bc7f67
Prajakta Kudale <prajaktakudale@INPrajakta.local>
2014-04-08 19:09:13 +0530

    testing integration.

--- demo1.rb
+++ demo1.rb
@@ -3,6 +3,7 @@ require 'mongoid'
 require 'json'
 require './model/employee.rb'
 require './model/juice_request.rb'
+
 Mongoid.load!("./config/mongoid.yml")
 
 get '/total/numberOfGlasses' do
@@ -12,9 +13,9 @@ end
 post '/employee/:id/drinks/:numberOfGlasses/glass' do
   	new_juice_request = JSON.parse(request.body.read)
   	emp_id = new_juice_request["employee_id"]
-  	numberOfGlasses = new_juice_request["numberOfGlasses"]
+  	numberOfGlasses = new_juice_request["numberOfGlasses"].to_i
+
   	date_time = new_juice_request["date_time"]
-  	 "#{:emp_id}-#{:numberOfGlasses}-#{:date_time}"
   	if(Employee.where({:employee_id => emp_id}).exists?)
   		Employee.find_by(employee_id: "15749").juice_requests << JuiceRequest.new({:no_of_glasses => numberOfGlasses,:date_time => date_time})
   	else
