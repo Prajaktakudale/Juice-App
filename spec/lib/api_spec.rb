9930e3cf5348b5010e2c93da7cd3fac922da8073
Prajakta Kudale <prajaktakudale@INPrajakta.local>
2014-04-21 11:30:34 +0530

    Date picker added

--- spec/lib/api_spec.rb
+++ spec/lib/api_spec.rb
@@ -6,26 +6,31 @@ describe 'API Test' do
     Employee.all.destroy
   end
 
-  it "total page should ok" do
+  it "total of number of glasses page should ok" do
     get "/total/numberOfGlasses"
     expect(last_response.status).to eq(200)
   end
 
-  it "total page should not ok" do
+  it "total of number of glasses page should not ok" do
     get "/total/numberOfGlasse"
     expect(last_response.status).to eq(404)
   end
 
-  it "total page should ok" do
+  it "juice consumtion page should ok" do
    Employee.count.should == 0
-   post '/employee/juice/comsumption', {:employee_id => "1574",:numberOfGlasses=>1,:date_time=>"Date"}.to_json
+   post '/employee/juice/comsumption', {:employee_id => "12345",:numberOfGlasses=>1,:date_time=>"Date"}.to_json
    Employee.count.should == 1
    expect(last_response.status).to eq(200)
  end
 
-   it "total page should not ok" do
-   post '/employee/juice/comsumptions', {:employee_id => "1574",:numberOfGlasses=>1,:date_time=>"Date"}.to_json
-   expect(last_response.status).to eq(404)
+ it "juice consumtion page should add one record in database" do
+   Employee.count.should == 0
+   post '/employee/juice/comsumption', {:employee_id => "23456",:numberOfGlasses=>1,:date_time=>"Date"}.to_json
+   Employee.count.should == 1
  end
 
+   it "juice consumtion page should not ok" do
+   post '/employee/juice/comsumptions', {:employee_id => "34567",:numberOfGlasses=>1,:date_time=>"Date"}.to_json
+   expect(last_response.status).to eq(404)
+ end
 end
